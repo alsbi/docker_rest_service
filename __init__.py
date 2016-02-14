1538c3d998ff14db21f83a78cc66bd5546d18add
alsbi <feanor4ik@gmail.com>
2016-02-14 20:47:27 +0300

    init

--- __init__.py
+++ __init__.py
@@ -15,6 +15,7 @@ from dockerlib import Base
 # for i in  Base.show_images():
 #    print i
 
+
 for i in Base.show_images_history('8693db7e8a0084b8aacba184cfc4ff9891924ed2270c6dec6a9d99bdcff0d1aa'):
     print i
 
